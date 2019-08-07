46e8eaea90f17185e0b1fc3fe61617bb4bde3d41
sravan <sravan@LAPTOP-QG4PIMTG>
2019-08-07 15:18:35 +0530

    Drivers added

--- sample/src/test/browser.java
+++ sample/src/test/browser.java
@@ -1,11 +1,21 @@
 package test;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class browser {
 
 	public static void main(String[] args) {
 		
-		System.out.println("hai");
-		System.out.println("Sujan_Manoj");
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\sravan\\Desktop\\GitRepository\\sample\\drivers\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.amazon.com/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.navigate().refresh();
+		driver.close();
 		
 	}
 
